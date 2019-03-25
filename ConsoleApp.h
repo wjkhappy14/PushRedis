00eab79a226874dd865c25e3778fd3cd1d3757fd
Angkor <wjk@youguwang.com.cn>
2019-03-25 11:17:44 +0800

    添加项目文件。

---
+++ ConsoleApp.h
@@ -0,0 +1,5 @@
+#pragma once
+namespace ConsoleApp
+{
+	int Pi = 246789975422222;
+}
---
+++ Quote.h
@@ -0,0 +1,34 @@
+#ifndef QUOTE_H
+#define QUOTE_H
+
+#include "TapQuoteAPI.h"
+#include "SimpleEvent.h"
+
+class Quote : public ITapQuoteAPINotify
+{
+public:
+	Quote(void);
+	~Quote(void);
+
+	void SetAPI(ITapQuoteAPI* pAPI);
+	void RunTest();
+
+public:
+	//对ITapQuoteAPINotify的实现
+	virtual void TAP_CDECL OnRspLogin(TAPIINT32 errorCode, const TapAPIQuotLoginRspInfo *info);
+	virtual void TAP_CDECL OnAPIReady();
+	virtual void TAP_CDECL OnDisconnect(TAPIINT32 reasonCode);
+	virtual void TAP_CDECL OnRspQryCommodity(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast, const TapAPIQuoteCommodityInfo *info);
+	virtual void TAP_CDECL OnRspQryContract(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast, const TapAPIQuoteContractInfo *info);
+	virtual void TAP_CDECL OnRspSubscribeQuote(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast, const TapAPIQuoteWhole *info);
+	virtual void TAP_CDECL OnRspUnSubscribeQuote(TAPIUINT32 sessionID, TAPIINT32 errorCode, TAPIYNFLAG isLast, const TapAPIContract *info);
+	virtual void TAP_CDECL OnRtnQuote(const TapAPIQuoteWhole *info);
+
+private:
+	ITapQuoteAPI* m_pAPI;
+	TAPIUINT32	m_uiSessionID;
+	SimpleEvent m_Event;
+	bool		m_bIsAPIReady;
+};
+
+#endif // QUOTE_H
