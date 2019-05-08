8c2299f1c4c95c39149c774ca7f0a9d844b3547f
wjk@youguwang.com.cn <wjk@youguwang.com.cn>
2019-05-08 10:43:52 +0800

    html

--- SignalR.TickService/App_Start/RouteConfig.cs
+++ SignalR.TickService/App_Start/RouteConfig.cs
@@ -21,7 +21,7 @@ namespace SignalR.Tick
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "App", action = "Main", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Main", id = UrlParameter.Optional }
             );
         }
     }
